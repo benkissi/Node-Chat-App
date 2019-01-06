61a3a0bf311564c73ea7e7b72e970027c2718b1c
benkissi <benkissi111@gmail.com>
2019-01-06 21:22:08 +0000

    added css for chat page

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
         console.log(message);
 
         io.emit('newMessage', generateMessage(message.from, message.text))
-        callback('This is from server')
+        callback();
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
